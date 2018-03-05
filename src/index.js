91c91d2ae1a5cd0e0c14233f3a0192535f025b57
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-03-05 13:41:39 -0800

    Initial scaffold

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// @flow
+
+import * as t from "@babel/types";
+
+type RefsHandlerT = (
+  t: Object,
+  context: Object,
+  refs: Object[],
+  specifierName: string,
+) => any;
+
+export function createNamedModuleVisitor(
+  moduleName: string | Array<string>,
+  packageName: string | Array<string>,
+  refsHandler: RefsHandlerT,
+) {
+  const compareToModuleName = Array.isArray(moduleName)
+    ? s => moduleName.includes(s)
+    : s => s === moduleName;
+  return {
+    /**
+     * Handle ES imports
+     *
+     * import {moduleName} from 'packageName';
+     */
+    ImportDeclaration(path /*: Object */, state /*: Object */) {
+      const sourceName = path.get("source").node.value;
+      if (
+        (Array.isArray(packageName) &&
+          packageName.indexOf(sourceName) === -1) ||
+        (typeof packageName === "string" && sourceName !== packageName)
+      ) {
+        return;
+      }
+      state.importedPackageName = sourceName;
+      path.get("specifiers").forEach(specifier => {
+        const localPath = specifier.get("local");
+        const localName = localPath.node.name;
+        const refPaths = localPath.scope.bindings[localName].referencePaths;
+        if (t.isImportSpecifier(specifier)) {
+          // import {moduleName} from 'packageName';
+          const specifierName = specifier.get("imported").node.name;
+          if (compareToModuleName(specifierName)) {
+            refsHandler(t, state, refPaths, specifierName);
+          }
+        } else if (t.isImportNamespaceSpecifier(specifier)) {
+          // import * as pkg from 'packageName';
+          // TODO: Handle this case, or issue a warning because this may not be 100% robust
+        } else if (t.isImportDefaultSpecifier(specifier)) {
+          if (compareToModuleName("default")) {
+            refsHandler(t, state, refPaths, "default");
+          }
+        }
+      });
+    },
+  };
+}
